777f9e70e7227abbae137f9eac01184da8268d89
GERALDINE <geraldineventurariveros@gmail.com>
2024-04-13 17:05:59 -0300

    Agregar archivos de proyecto.

---
+++ CLASE_SOBRECARGA/Equipo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASE_SOBRECARGA
+{
+    internal class Equipo
+    {
+        public Equipo(string nombre, DateTime fundacion, int campeonatos, int seguidores)
+        {
+            this.nombre = nombre;
+            this.fundacion = fundacion;
+            this.campeonatos = campeonatos;
+            this.seguidores = seguidores;
+        }
+
+        public string nombre { get; set; }
+        public DateTime fundacion { get; set; }
+        public int campeonatos { get; set; }
+        public int seguidores { get; set; }
+
+
+
+        public static bool operator <(Equipo a, Equipo b) => a.seguidores < b.seguidores;
+
+        public static bool operator >(Equipo a, Equipo b) => a.seguidores > b.seguidores;
+
+        public override string ToString()
+        {
+            return String.Format($" {nombre} , fundacion {fundacion.Year} , campeonatos {campeonatos}, seguidores {seguidores}");
+        }
+
+        
+
+      
+    } 
+    
+}
---
+++ CLASE_SOBRECARGA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASE_SOBRECARGA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var America = new Equipo("Americano", new DateTime(2024,02,15), 5, 122000);
+            var Argentino = new Equipo("Argentino", new DateTime(2000,10,1), 5, 200);
+            var Chileno = new Equipo("Chileno", new DateTime(2003,11,30), 5, 3000);
+           
+            Console.WriteLine(America);
+            Console.WriteLine(Argentino);
+            Console.WriteLine(Chileno);
+           
+            if (Argentino > Chileno)
+            {
+                Console.WriteLine(America);
+            }
+
+            else
+            {
+                Console.WriteLine(Argentino);
+                Console.WriteLine(Chileno);
+            }
+                
+            
+        }
+    }
+}
